ab051f79300c5f5776ec7bb24ad9cc24a9bf49ca
Dalton Miller <deqlaredalton@gmail.com>
2022-03-23 20:43:53 -0500

    first commit

---
+++ src/main/java/com/daltonkyemiller/daltonkyemillerapi/DaltonkyemillerApiApplication.java
@@ -0,0 +1,13 @@
+package com.daltonkyemiller.daltonkyemillerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaltonkyemillerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaltonkyemillerApiApplication.class, args);
+	}
+
+}
