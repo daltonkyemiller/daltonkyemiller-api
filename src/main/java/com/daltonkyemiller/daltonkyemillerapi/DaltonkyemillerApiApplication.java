bbf598509a19ead1617a89f42048114fd5007f91
Dalton Miller <deqlaredalton@gmail.com>
2022-04-05 15:22:27 -0500

    refactor(properties): Removing secret keys from properties for production

--- src/main/java/com/daltonkyemiller/daltonkyemillerapi/DaltonkyemillerApiApplication.java
+++ src/main/java/com/daltonkyemiller/daltonkyemillerapi/DaltonkyemillerApiApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
- @SpringBootApplication
+@SpringBootApplication
 @EnableMongoAuditing
 public class DaltonkyemillerApiApplication{
-
 	 public static void main(String[] args) {
 		SpringApplication.run(DaltonkyemillerApiApplication.class, args);
 	}
