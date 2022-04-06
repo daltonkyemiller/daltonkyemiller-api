023cda592f0599bdd487a2f265f2f7c4108b75f7
Dalton Miller <deqlaredalton@gmail.com>
2022-04-06 12:54:24 -0500

    feat(Security): implementing Jasypt for encryption...will make easier for CI/CD

--- src/main/java/com/daltonkyemiller/daltonkyemillerapi/DaltonkyemillerApiApplication.java
+++ src/main/java/com/daltonkyemiller/daltonkyemillerapi/DaltonkyemillerApiApplication.java
@@ -1,11 +1,14 @@
  package com.daltonkyemiller.daltonkyemillerapi;
 
 
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
+@EnableEncryptableProperties
 @SpringBootApplication
 @EnableMongoAuditing
 public class DaltonkyemillerApiApplication{
